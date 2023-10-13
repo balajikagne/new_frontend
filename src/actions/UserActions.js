1f592422c03a34099e2f285d6af6c2bbf0bad44a
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-10-14 03:18:43 +0530

    Update UserActions.js bnhj
    fjfk

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export const registerUser=(user)=>async dispatch=>{
     dispatch({type:'USER_REGISTER_REQ'})
     try{
-        const response=await axios.post('https://balajibackend-demo-1.onrender.com//api/users/register',user)
+        const response=await axios.post('https://balajibackend-demo-1.onrender.com/api/users/register',user)
         
         dispatch({type:'USER_REGISTER_SUCCESS'})
     }
@@ -15,7 +15,7 @@ export const loginUser=(user)=>async dispatch=>{
     dispatch({type:'USER_LOGIN_REQ'})
 
     try{
-        const response=await axios.post('https://balajibackend-demo-1.onrender.com//api/users/login',user)
+        const response=await axios.post('https://balajibackend-demo-1.onrender.com/api/users/login',user)
         // console.log(response)
         dispatch({type:'USER_LOGIN_SUCCESS',payload: response.data})
         localStorage.setItem('currentUser',JSON.stringify(response.data))
