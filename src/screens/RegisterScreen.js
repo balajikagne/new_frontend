5fe3b083511fb48d93f0cd07808b69a2606f574f
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-10-14 15:00:09 +0530

    Update RegisterScreen.js bkh
    ihui

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -1,64 +1,92 @@
-import React from 'react'
-import { useState,useEffect } from 'react';
-import { loginUser } from '../actions/UserActions';
-import { useDispatch, useSelector } from 'react-redux';
-import Loading from '../components/Loading';
-import Error from '../components/Error';
-
-export default function LoginScreen() {
-
+import React from "react";
+import { useState, useEffect } from "react";
+import {useDispatch,useSelector} from 'react-redux';
+import { registerUser } from "../actions/UserActions";
+import Success from "../components/Success";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+export default function RegisterScreen() {
+  const [name, setname] = useState("");
   const [mobNumber, setmobNumber] = useState("");
   const [password, setpassword] = useState("");
-  const loginstate=useSelector(state=>state.loginUserReducer)
-  const {loading,error}=loginstate;
+  const [cpassword,setcpassword]=useState("")
+  const registerstate=useSelector(state=>state.registerUserReducer)
+  const {error,loading,success}=registerstate
   const dispatch=useDispatch()
-  
-  useEffect(()=>{
-    if (localStorage.getItem('currentUser'))
+  function register(){
+    if (password!=cpassword)
     {
-      window.location.href='/'
+        alert("passwords not matched")
     }
-  })
-  function login(){
-    const user={mobNumber,password}
-    dispatch(loginUser(user))
+    else
+    {
+        const user={
+            name,
+            mobNumber,
+            password
+        }
+        // console.log(user)
+        dispatch(registerUser(user))
+    }
   }
   return (
     <div>
-    <div className="row justify-content-center mt-5 ">
-      <div className="col-md-5 mt-5 text-left shadow-lg p-3 mb-5 bg-white rounded">
-        <h3 className="text-center m-2">
-          Login
-        </h3>
-        {loading && (<Loading/>)}
-        {error &&(<Error error='Something went wrong'/>)}
-        <div>
-          <input
-            type="text"
-            placeholder="Mob Number"
-            className="form-control"
-            value={mobNumber}
-            onChange={(e)=>{
-              setmobNumber(e.target.value)
-            }}
-            required
-          ></input>
-          <input
-            type="password"
-            placeholder="password"
-            className="form-control"
-            value={password}
-            onChange={(e)=>{
-              setpassword(e.target.value)
-            }}
-            required
-          ></input>
-          <button onClick={login} className="btn mt-3">Login</button>
-          <br></br>
-          <a style={{color:'black',textDecoration:'none'}} href="/register" className='mt-2'>Click Here To register</a>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5 text-left">
+          {loading && (<Loading/>)}
+          {success && (<Success success='User Register Successfully'/>)}
+          {error && (<Error error="mobNumber already register"/>)}
+          <h3 className="text-center m-2" style={{fontSize:'35px'}}>
+            Register
+          </h3>
+          <div>
+            <input
+              type="text"
+              placeholder="Full Name"
+              className="form-control"
+              value={name}
+              onChange={(e)=>{
+                setname(e.target.value)
+              }}
+              required
+            ></input>
+            <input
+              type="text"
+              placeholder="Mob Number"
+              className="form-control"
+              value={mobNumber}
+              onChange={(e)=>{
+                setmobNumber(e.target.value)
+              }}
+              required
+            ></input>
+            <input
+              type="password"
+              placeholder="password"
+              className="form-control"
+              value={password}
+              onChange={(e)=>{
+                setpassword(e.target.value)
+              }}
+              required
+            ></input>
+            <input
+              type="password"
+              placeholder="comfirmpassword"
+              className="form-control"
+              value={cpassword}
+              onChange={(e)=>{
+                setcpassword(e.target.value)
+              }}
+              required
+            ></input>
+            <button onClick={register} className="btn mt-3">Register</button>
+            <br></br>
+            <a style={{color:'black',textDecoration:'none'}} href="/login" className='mt-2'>Click Here To Login</a>
+          </div>
         </div>
       </div>
+      
     </div>
-  </div>
-  )
+  );
 }
